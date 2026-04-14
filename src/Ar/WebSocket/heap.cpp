617b5097ce7183b4e30d88f793eb47efdacf0468
Josh Polansky <josh@loupe.team>
2026-04-14 12:14:46 -0700

    Add heap size

---
+++ src/Ar/WebSocket/heap.cpp
@@ -0,0 +1,11 @@
+/*
+ * File: heap.cpp
+ * Copyright (c) 2023 Loupe
+ * https://loupe.team
+ * 
+ * This file is part of LLHttp, licensed under the MIT License.
+ */
+
+//Dummy file to invoke the cpp compiler for malloc/free
+unsigned long bur_heap_size = 0xFFFFF;
+
